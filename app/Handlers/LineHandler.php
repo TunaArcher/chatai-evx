bce05ccc0ae2c848297a22996444f66a753eb302
Panumas Srisook <Panumas.srisook@gmail.com>
2025-01-30 18:57:09 +0700

    log6

--- app/Handlers/LineHandler.php
+++ app/Handlers/LineHandler.php
@@ -94,7 +94,7 @@ class LineHandler
         $UID = $customer->uid;
 
         $messages = $this->messageModel->getMessageNotReplyBySendByAndRoomID('Customer', $messageRoom->id);
-        log_message("info", "message_type_up: " . $messages);
+        log_message("info", "message_type_up: ");
         $message = $this->getUserContext($messages);
         log_message("info", "message_type_down: " . $message['message']);
 
