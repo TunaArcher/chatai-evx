b3f097342fc5efaf0f29496bf9370736be0db942
TunaArcher <9tnk.sl@gmail.com>
2025-01-31 13:17:37 +0700

    update

--- app/Views/auth/signup.php
+++ app/Views/auth/signup.php
@@ -2,6 +2,7 @@
 <html lang="th">
 
 <head>
+  <meta name="facebook-domain-verification" content="hmgpdjforybq3d41qxbf3thstnf3z4" />
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>AutoConX | เข้าสู่ระบบ</title>
